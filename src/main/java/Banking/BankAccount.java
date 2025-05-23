38fe80ff5e5e6dd73a5200c021fb7f5589c68c94
Yekta <yektaramzannasab@gmail.com>
2025-05-24 00:33:38 +0330

    first version

--- src/main/java/Banking/BankAccount.java
+++ src/main/java/Banking/BankAccount.java
@@ -17,25 +17,59 @@ public class BankAccount {
         return  id;
     }
     public int getBalance() {
-        // TODO: Consider locking (if needed)
-        return balance;
+        // we are just reading, we do not strictly need locking here; but for consistency, we will use it
+        lock.lock();
+        try {
+            return balance;
+        } finally {
+            lock.unlock();
+        }
     }
 
-    public Lock getLock() {
-        return lock;
-    }
 
     public void deposit(int amount) {
-        // TODO: Safely add to balance.
+        lock.lock();
+        try {
+            balance += amount;
+        } finally {
+            lock.unlock();
+
+        }
     }
 
     public void withdraw(int amount) {
-        // TODO: Safely withdraw from balance.
+        lock.lock();
+        try {
+            balance -= amount;
+        } finally {
+            lock.unlock();
+
+        }
     }
 
     public void transfer(BankAccount target, int amount) {
         // TODO: Safely make the changes
         // HINT: Both accounts need to be locked, while the changes are being made
         // HINT: Be cautious of potential deadlocks.
+        //To prevent deadlocks, we will always lock accounts in a consistent order
+        //We will use account ID to determine  the locking order
+
+        BankAccount first = this.id < target.id ? this : target;
+        BankAccount second = this.id < target.id ? target : this;
+
+        first.lock.lock();
+        try {
+            second.lock.lock();
+            try {
+                this.balance -= amount;
+                target.balance += amount;
+            } finally {
+                second.lock.unlock();
+            }
+        } finally {
+            first.lock.unlock();
+        }
+
+
     }
 }
